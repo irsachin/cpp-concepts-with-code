b99bb6aa1b02d38458ccbf2736a406bd2da5c430
ShivaniSugandhi <shivanisugandhi009@gmail.com>
2025-06-03 22:18:59 +0530

    last commit

---
+++ C++ Introduction & Syntax/C++_Syntax.cpp
@@ -0,0 +1,12 @@
+// c++ Syntax
+//Header file
+#include <iostream> //preprocessor directive include input output stream library 
+using namespace std; //
+
+int main() {
+    cout<<"Welcome to irsachin github profile";
+    return 0;
+}
+
+//let's break code for understanding 
+
--- C++_Strings.cpp
+++
@@ -1,9 +0,0 @@
-C++ Strings = Strings are used for storing text/characters.
-string variable contains 
-
-#include<iostream>
-using namespace std;
-int main()
-{
-
-}
